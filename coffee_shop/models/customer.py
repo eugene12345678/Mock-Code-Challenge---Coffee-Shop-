7f2f147e8be9cb5c77994a7b4c176d162fa5eb4f
Eugene Mathenge <eugene.mukabi@student.moringaschool.com>
2024-09-08 12:01:00 +0300

    completed Object Relationship Methods and Properties in customer

--- coffee_shop/models/customer.py
+++ coffee_shop/models/customer.py
@@ -1,6 +1,8 @@
 class Customer:
     def __init__(self, name):
         self.name = name
+        self._orders = []
+    
     @property
     def name(self)  :
         return self.value 
@@ -11,6 +13,20 @@ class Customer:
         else:
             raise ValueError("Name must be a string between 1 and 15")
         
-name1 = Customer("eugene")
-print(name1.name)
-        
+# name1 = Customer("eugene")
+# print(name1.name)
+    def orders(self):
+        return self._orders
+     
+    def coffees(self):
+        unique_coffees = []
+        for order in self._orders:
+            if order.coffee not in unique_coffees:
+                unique_coffees.append(order.coffee)
+        return unique_coffees
+    
+    def new_order(self, coffee, price):
+        order = Order(self, coffee, price)
+        self._orders.append(order)
+        coffee._orders.append(order)
+        return order
