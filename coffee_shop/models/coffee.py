b596ccdb1d4957264f71488fede20a58e58c19f4
Eugene Mathenge <eugene.mukabi@student.moringaschool.com>
2024-09-08 12:10:34 +0300

    completed Object Relationship Methods and Properties in order

--- coffee_shop/models/coffee.py
+++ coffee_shop/models/coffee.py
@@ -1,7 +1,7 @@
 class Coffee:
     def __init__(self, name):
          self.name = name
-         
+         self._orders = []
     @property
     def name (self):
         return self.value
@@ -11,5 +11,17 @@ class Coffee:
             self.value = value
         else:
             raise ValueError("Name must be a string with at least 3 characters")
-coffee1 = Coffee("coffee")
-print(coffee1.name)
+# coffee1 = Coffee("coffee")
+# print(coffee1.name)
+
+    def orders(self):
+        return self._orders
+    
+    def customers(self):
+    # Returns a list of all customers who have placed orders for this coffee
+        all_customers = []
+        for order in self._orders:
+          if order.customer not in all_customers:
+            all_customers.append(order.customer)
+        return all_customers
+
